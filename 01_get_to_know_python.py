6d8fbccea8281f72caa8764ec3dcb7c798ee15e6
dragon <ganglongy@pku.edu.cn>
2021-06-05 23:04:52 +0800

    Zen, sysinfo, comment, print, turtle

---
+++ 01_get_to_know_python.py
@@ -0,0 +1,28 @@
+import this # zen of python
+import turtle
+import sys
+
+# version info
+print(sys.version_info)
+print(sys.version)
+
+'''
+multi-line
+comments
+'''
+print('hello word')
+
+# draw
+turtle.pensize(4)
+turtle.pencolor('red')
+
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+
+turtle.mainloop()
+
